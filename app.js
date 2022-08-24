d080e403885def26b0cc8d996eb37ebf6f7a1111
Nidhi Sharma <110181299+nidhi7485@users.noreply.github.com>
2022-08-25 01:07:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,17 +8,33 @@
 // const hash = MD5(
 //   "1d2394099377caf51ee8b9b0572bec256d728c3fcd23e756ccd9401360001457f69b812c2"
 // ).toString();
+
+// selecting all the element
 const allTabsBody = document.querySelectorAll(".tab-body-single");
 const allTabsHead = document.querySelectorAll(".tab-head-single");
 const searchForm = document.querySelector(".app-header-search");
+const btnsOpenModal = document.querySelector(".show-modal");
+const btnCloseModal = document.querySelector(".close-modal");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
 let searchList = document.getElementById("search-list");
+
+// variables for modal
+let singleData;
+let store = "";
+let favArray = [];
+let deleteBtn;
+let favAdded = false;
 
 let activeTab = 1,
   allData;
 
+
+  // init function
 const init = () => {
   showActiveTabBody();
   showActiveTabHead();
+  loadFavlist();
 };
 
 const showActiveTabHead = () =>
@@ -38,9 +54,54 @@ const hideAllTabHead = () =>
     singleTabHead.classList.remove("active-tab")
   );
 
-// even listeners
+//      modal functions
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+  // loadFavlist()
+};
+
+// close modal
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+// favlist function
+const loadFavlist = () => {
+  let temp = localStorage.getItem("hero");
+  if (!temp) return;
+  favArray = [];
+  favArray = temp.split(",");
+  favArray = [...new Set(favArray)];
+  console.log(favArray);
+  console.log(temp);
+
+  favArray.forEach(async function (item) {
+    const result = await fetch(
+      `https://www.superheroapi.com/api.php/727054372039115/${item}`
+    );
+
+    // https://www.superheroapi.com/api.php/727054372039115/search/${searchText}
+    const heroDetails = await result.json();
+    // console.log(heroDetails)
+    const divElem = document.createElement("div");
+    divElem.classList.add("search-list-item");
+    divElem.innerHTML = `
+            <img src = "${
+              heroDetails.image.url ? heroDetails.image.url : ""
+            }" alt = "">
+            <p data-id = "${heroDetails.id}">${heroDetails.name}</p>
+            <button class = "modal-btn del-fav">delete</button>
+             `;
+    modal.insertAdjacentElement("beforeend", divElem);
+  });
+};
+
+// event listeners
 window.addEventListener("DOMContentLoaded", () => init());
-// button event listeners
+
+// for each on all tabs
 allTabsHead.forEach((singleTabHead) => {
   singleTabHead.addEventListener("click", () => {
     hideAllTabHead();
@@ -49,7 +110,12 @@ allTabsHead.forEach((singleTabHead) => {
     showActiveTabBody();
   });
 });
+// open modal
+btnsOpenModal.addEventListener("click", openModal);
 
+// close modal
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
 const getInputValue = (event) => {
   event.preventDefault();
   let searchText = searchForm.search.value;
@@ -74,6 +140,7 @@ const fetchAllSuperHero = async (searchText) => {
   }
 };
 
+// show search list
 const showSearchList = (data) => {
   searchList.innerHTML = "";
   data.forEach((dataItem) => {
@@ -88,7 +155,7 @@ const showSearchList = (data) => {
     searchList.appendChild(divElem);
   });
 };
-
+// event listner on search form
 searchForm.search.addEventListener("keyup", () => {
   if (searchForm.search.value.length > 1) {
     fetchAllSuperHero(searchForm.search.value);
@@ -97,22 +164,26 @@ searchForm.search.addEventListener("keyup", () => {
   }
 });
 
+
+// evenlistner on searchlist
 searchList.addEventListener("click", (event) => {
   let searchId = event.target.dataset.id;
-  let singleData = allData.results.filter((singleData) => {
+  singleData = allData.results.filter((singleData) => {
     return searchId === singleData.id;
   });
   showSuperheroDetails(singleData);
   searchList.innerHTML = "";
 });
 
+
+// function for show superhero details
 const showSuperheroDetails = (data) => {
-  console.log(data);
   document.querySelector(".app-body-content-thumbnail").innerHTML = `
         <img src = "${data[0].image.url}">
     `;
-
+  document.querySelector(".app-body-content").id = data[0].id;
   document.querySelector(".name").textContent = data[0].name;
+
   document.querySelector(".powerstats").innerHTML = `
     <li>
         <div>
@@ -235,3 +306,71 @@ const showSuperheroDetails = (data) => {
     </li>
     `;
 };
+const alert = document.querySelector(".alert");
+const heart = document.querySelector(".fa-heart");
+const favBtn = document.querySelector(".fav-btn");
+
+// eventlistner on favourite button
+favBtn.addEventListener("click", (e) => {
+  heart.style.color = "red";
+  alert.textContent = "added to favourites";
+  alert.classList.add("sucess");
+  setTimeout(function () {
+    alert.textContent = "";
+    alert.classList.remove("sucess");
+    heart.style.color = "white";
+  }, 2000);
+  // if (favAdded) return
+  let temp = localStorage.getItem("movie");
+  // console.log(temp);
+  if (temp) {
+    favArray = temp.split(",");
+  }
+  favArray = [...new Set(favArray)];
+  console.log(singleData);
+  if (favArray.includes(singleData[0].id)) return;
+  const divElem = document.createElement("div");
+  divElem.classList.add("search-list-item");
+  // divElem.setAttribute('data-id', singleData[0].id)
+  divElem.innerHTML = `
+            <img src = "${
+              singleData[0].image.url ? singleData[0].image.url : ""
+            }" alt = "">
+            <p data-id = "${singleData[0].id}">${singleData[0].name}</p>
+            <button class = "modal-btn del-fav">delete</button>
+             `;
+  modal.insertAdjacentElement("beforeend", divElem);
+  // favAdded = true
+  favArray.push(singleData[0].id);
+
+  favArray = [...new Set(favArray)];
+  store = favArray.toString();
+  // console.log(favArray);
+  localStorage.setItem("hero", store);
+});
+
+//     modal list items event handlers
+modal.addEventListener("click", async function (e) {
+  if (e.target.closest(".search-list-item")) {
+    const element = e.target.closest(".search-list-item");
+    const id = element.children.item(1).dataset.id;
+    if (e.target.classList.contains("del-fav")) {
+      this.removeChild(element);
+      favAdded = false;
+      favArray.forEach(function (value, i) {
+        if (value === id) {
+          favArray.splice(i, 1);
+        }
+      });
+      favArray = [...new Set(favArray)];
+      console.log(favArray);
+      let store = favArray.toString();
+      localStorage.setItem("hero", store);
+      if (!localStorage.getItem("hero")) {
+        localStorage.clear();
+      }
+    } else {
+      closeModal();
+    }
+  }
+});
